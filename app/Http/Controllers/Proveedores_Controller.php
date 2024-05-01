413ef2398d56a951cc1858356e85f64b7a51398e
Robert Hernández <143993982+RobertHC0809@users.noreply.github.com>
2024-05-01 15:04:37 -0700

    projrct

---
+++ app/Http/Controllers/Proveedores_Controller.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Proveedores_Controller extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/Reservas_Controller.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Reservas_Controller extends Controller
+{
+    //
+}
